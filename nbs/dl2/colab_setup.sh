71f9e08a5dd18e1fa76d9298aa353ebcb86fb906
Jaime Durán <iamxami@gmail.com>
2020-01-10 22:48:17 +0100

    Colab setup

--- nbs/dl2/colab_setup.sh
+++ nbs/dl2/colab_setup.sh
@@ -1,6 +1,7 @@
-
+#!/bin/bash
 pip install https://download.pytorch.org/whl/cu100/torch-1.2.0-cp36-cp36m-manylinux1_x86_64.whl
 pip install https://download.pytorch.org/whl/cu100/torchvision-0.4.0-cp36-cp36m-manylinux1_x86_64.whl
+
 rm -rf apex
 git clone https://github.com/NVIDIA/apex
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./apex
